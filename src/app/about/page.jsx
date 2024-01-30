59a18fe6849d9c10bd786d7b9c665b7b229f9ddb
Berkay Anduv <sygrok@hotmail.com>
2024-01-30 18:53:56 +0300

    nextjs-images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="about page image"
+          fill
+          // width={500}
+          // height={500}
+        />
+      </div>
+    </>
+  );
 }
 
 export default AboutPage;
